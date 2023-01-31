06ac7228ac0b90c5e1fc62af4015a1a218cea06c
CoccodrillooXDS <coccodrillo.0001@gmail.com>
2023-01-31 17:06:46 +0100

    Added Arduino sketch

---
+++ arduino/arduino.ino
@@ -0,0 +1,33 @@
+// ------------------------------------------------------------
+// Working with a GPS Module and Arduino
+// GPS Module: NEO-6M
+// Arduino: Arduino Uno
+// ------------------------------------------------------------
+
+
+#include <SoftwareSerial.h>
+// Include the SoftwareSerial library to allow the Arduino to communicate with the GPS module.
+
+// Define the pins that will be used to communicate with the GPS module.
+int RXPin = 4;
+int TXPin = 3;
+
+// Define the baud rate that will be used to communicate with the GPS module.
+int GPSBaud = 9600;
+
+// Create a new SoftwareSerial object called GPSSerial.
+SoftwareSerial GPSSerial(RXPin,TXPin);
+
+void setup() {
+  // Initialize the serial communication with the computer and the GPS module.
+  Serial.begin(9600);
+  GPSSerial.begin(9600);
+}
+
+void loop() {
+  // If the GPS module is connected and it's working, read and send the data to the computer.
+  while (GPSSerial.available() > 0) {
+    byte gpsData = GPSSerial.read();
+    Serial.write(gpsData);
+  }
+}
