9365f8ccb2c7126bf24a4a8b8282af6fc2d59685
CoccodrillooXDS <coccodrillo.0001@gmail.com>
2023-02-03 17:46:55 +0100

    Removed a filter that broke the gps position

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var Sylvester = require('sylvester');
-var Kalman = require('kalman').KF;
-
 const colors = require('colors');
 const SerialPort = require('serialport');
 const parsers = SerialPort.parsers;
@@ -16,19 +13,6 @@ var port = '';
 
 var GPS = require('gps');
 var gps = new GPS;
-
-// Simple Kalman Filter set up
-var A = Sylvester.Matrix.I(2);
-var B = Sylvester.Matrix.Zero(2, 2);
-var H = Sylvester.Matrix.I(2);
-var C = Sylvester.Matrix.I(2);
-var Q = Sylvester.Matrix.I(2).multiply(1e-11);
-var R = Sylvester.Matrix.I(2).multiply(0.00001);
-
-// Measure
-var u = $V([0, 0]);
-
-var filter = new Kalman($V([0, 0]), $M([[1, 0], [0, 1]]));
 
 var portstatus = {
     port: '',
@@ -83,32 +67,13 @@ function serportListener(port) {
 
 gps.on('data', function(data) {
     if (data.lat && data.lon) {
-        filter.update({
-            A: A,
-            B: B,
-            C: C,
-            H: H,
-            R: R,
-            Q: Q,
-            u: u,
-            y: $V([data.lat, data.lon])
-        });
-        gps.state.position = {
-            cov: filter.P.elements,
-            pos: filter.x.elements
-        };
-    }
-    io.emit('position', gps.state);
-});
-
-gps.on('GGA', function(data) {
-    if (data.lat && data.lon) {
         portstatus.gpsData = true;
         io.emit('portstatus', portstatus);
     } else{
         portstatus.gpsData = false;
         io.emit('portstatus', portstatus);
     }
+    io.emit('position', gps.state);
 });
 
 app.use(express.json());
@@ -154,7 +119,7 @@ io.on('connection', function(socket) {
         if (serport.isOpen) {
             var portstatus = {
                 port: port,
-                status: 'connected'
+                status: 'Connected'
             }
             io.emit('portstatus', portstatus);
         }
